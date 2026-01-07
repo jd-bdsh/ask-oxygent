5dde118ad90bd2349e8e0a2a4a1a33cac3b53f1e
heyongqi10 <heyongqi10@jd.com>
2026-01-07 16:05:51 +0800

    feat: add the python agent in demo

---
+++ demo/agent/main.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from oxygent import MAS, oxy
+import os
+
+master_prompt = """
+你是一个数学家，能够帮助用户解决数学问题，请为用户提供计算结果。
+"""
+
+oxy_space = [
+    oxy.HttpLLM(
+        name="default_llm",
+        api_key=os.getenv("DEFAULT_LLM_API_KEY"),
+        base_url=os.getenv("DEFAULT_LLM_BASE_URL"),
+        model_name=os.getenv("DEFAULT_LLM_MODEL_NAME"),
+        llm_params={"temperature": 0.01},
+        semaphore=4, # 并发量
+        timeout=240, # 最大执行时间
+    ),
+    oxy.ReActAgent(
+        name="master_agent",
+        prompt = master_prompt,
+        is_master=True,
+        llm_model="default_llm",
+    ),
+]
+
+
+async def main():
+    async with MAS(oxy_space=oxy_space) as mas:
+        await mas.start_web_service(
+            first_query="Hello!"
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
